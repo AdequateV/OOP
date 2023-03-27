2954527dd0a1015593eac2c06af3c5ff8305f149
Vadim <48867558+AdequateV@users.noreply.github.com>
2023-03-27 16:50:11 +0500

    Add files via upload

---
+++ CirclesEditorGui/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Lab4_wforms_oop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBoxJoints = new System.Windows.Forms.CheckBox();
+            this.checkBoxMulti = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 327);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 426);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "[Ctrl]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 31);
+            this.panel1.TabIndex = 0;
+            // 
+            // checkBoxJoints
+            // 
+            this.checkBoxJoints.AutoSize = true;
+            this.checkBoxJoints.Checked = true;
+            this.checkBoxJoints.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxJoints.Location = new System.Drawing.Point(348, 397);
+            this.checkBoxJoints.Name = "checkBoxJoints";
+            this.checkBoxJoints.Size = new System.Drawing.Size(134, 19);
+            this.checkBoxJoints.TabIndex = 4;
+            this.checkBoxJoints.TabStop = false;
+            this.checkBoxJoints.Text = "select all at joints [Z]";
+            this.checkBoxJoints.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMulti
+            // 
+            this.checkBoxMulti.AutoSize = true;
+            this.checkBoxMulti.Checked = true;
+            this.checkBoxMulti.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxMulti.Location = new System.Drawing.Point(348, 419);
+            this.checkBoxMulti.Name = "checkBoxMulti";
+            this.checkBoxMulti.Size = new System.Drawing.Size(162, 19);
+            this.checkBoxMulti.TabIndex = 5;
+            this.checkBoxMulti.TabStop = false;
+            this.checkBoxMulti.Text = "multi select [X] (hold Ctrl)";
+            this.checkBoxMulti.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(656, 426);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tip: use R to deselect all";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBoxMulti);
+            this.Controls.Add(this.checkBoxJoints);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private CheckBox checkBoxJoints;
+        private CheckBox checkBoxMulti;
+        private Label label3;
+    }
+}
