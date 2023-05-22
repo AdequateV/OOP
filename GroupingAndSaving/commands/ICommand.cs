83cfbb92957d8f265d74fa2dd4d37a80c5aea896
Vadim <48867558+adequatev@users.noreply.github.com>
2023-05-22 15:14:21 +0500

    added group and ungroup commands

--- GroupingAndSaving/commands/ICommand.cs
+++ GroupingAndSaving/commands/ICommand.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lab7_oop;
 
 namespace Lab7_oop
 {
@@ -114,47 +115,81 @@ namespace Lab7_oop
 
     public class GroupCommand : ICommand
     {
+        public List<IShape> shapes;
+        public IShape added;
+
+        public GroupCommand(List<IShape> shapes, IShape added = null)
+        {
+            this.shapes = shapes;
+            this.added = added;
+        }
+
         public override ICommand clone()
         {
-            throw new NotImplementedException();
+            return new GroupCommand(shapes);
         }
 
         public override void execute(IShape shape)
         {
-            throw new NotImplementedException();
+            if (shape == null) return;
+            added = shape;
+            ((shapeGroup)added).shapes.Clear();
+            foreach (var s in shapes)
+                if (s.IsSelected) ((shapeGroup)added).AddShape(s);
+            List<IShape> b = new();
+
+            foreach (var s in shapes)
+                if (s.IsSelected) b.Add(s);
+            foreach (var s in b)
+                shapes.Remove(s);
+            if(((shapeGroup)added).shapes.Count > 0)
+            {
+                shapes.Add(added);
+            }
+            
         }
 
-        public override IShape GetShape()
-        {
-            throw new NotImplementedException();
-        }
+        public override IShape GetShape() => added;
 
         public override void unexecute()
         {
-            throw new NotImplementedException();
+            List<IShape> grElems = new();
+            ((shapeGroup)added).Ungroup(grElems);
+            shapes.Remove(added);
+            foreach (var s in grElems)
+                shapes.Add(s);
+                
         }
     }
 
     public class UnGroupCommand : ICommand
     {
+        public List<IShape> shapes;
+        public IShape added;
+        public UnGroupCommand(List<IShape> _selection)
+        {
+            shapes = _selection;
+        }
+
         public override ICommand clone()
         {
-            throw new NotImplementedException();
+            return new UnGroupCommand(shapes);
         }
 
         public override void execute(IShape shape)
         {
-            throw new NotImplementedException();
+            added = shape;
+            ICommand c = new GroupCommand(shapes, added);
+            c.unexecute();
         }
 
-        public override IShape GetShape()
-        {
-            throw new NotImplementedException();
-        }
+        public override IShape GetShape() => added;           
+        
 
         public override void unexecute()
         {
-            throw new NotImplementedException();
+            ICommand c = new GroupCommand(shapes);
+            c.execute(added);
         }
     }
 
@@ -234,5 +269,6 @@ namespace Lab7_oop
 
         }
     }
+
 
 }
